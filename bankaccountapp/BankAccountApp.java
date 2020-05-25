5b3b77bbe573890b18e6e952354d4a85dda7e18e
unknown <sdgerbich@gmail.com>
2020-05-25 16:02:13 -0400

    seperated into account type

--- bankaccountapp/BankAccountApp.java
+++ bankaccountapp/BankAccountApp.java
@@ -1,9 +1,14 @@
 package bankaccountapp;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
     public static void main(String[] args) {
+
+        List<Account> accounts = new LinkedList<Account>();
+
+
         String file = "C:\\Users\\admin\\Documents\\HW stuff\\JAVA\\day9\\CustList.csv";
     //    Checking chkacc1 = new Checking("Tom Wilson","123456789", 1500);
        
@@ -17,9 +22,26 @@ public class BankAccountApp {
 
         List<String[]> newAccountHolder = utilities.CSV.read(file);
         for (String[] accountHolder : newAccountHolder){
-            System.out.println(accountHolder[0]);
-            System.out.println(accountHolder[1]);
-            System.out.println(accountHolder[2]);
+            String name = accountHolder[0];
+            String sSN = accountHolder[1];
+            String accountType = accountHolder[2];
+            double initDeposit= Double.parseDouble(accountHolder[3]);
+            // System.out.println(name + " " + sSN + " " + accountType+ " $ "+initDeposit);
+            if(accountType.equals("Savings")){
+                
+                accounts.add(new Savings(name, sSN, initDeposit));
+            }else if(accountType.equals("Checking")) {
+                
+                accounts.add(new Checking(name, sSN, initDeposit));
+            }else {
+                System.out.println("ERROR reading account type");
+            }
         }
+
+        for(Account acc : accounts){
+            System.out.println("*******************\n");
+            acc.showInfo();
+        }
+
     }
 }
